cc8a25764aa09227073dd20e0dc8946ab2b068e2
August <august.enhager@gmail.com>
2021-09-21 14:31:56 +0200

    addds creation off articles in index_spec

--- spec/requests/api/articles/index_spec.rb
+++ spec/requests/api/articles/index_spec.rb
@@ -1,14 +1,26 @@
 RSpec.describe 'GET /api/articles', type: :request do
-subject { response }
+  
+  subject { response }
+  
+  describe 'when there are some articles in the database' do
+    let!(:articles) do
+      create_list(:article, 2)
+      create(:article, title: 'My amazing news article')
+    end
+    before do      
+      get '/api/articles'
+    end
 
-  before do
-    get '/api/articles'
-  end
-
-  xdescribe 'when there are some articles in the database' do
+    it 'is expected to return a collection of articles' do
+      expect(response_json['articles'].count).to eq 3
+    end
   end
 
   describe 'when there are NO articles in the database' do
+    before do
+      get '/api/articles'
+    end
+
     it { is_expected.to have_http_status 200 }
 
     it 'is expected to respond with a message' do
